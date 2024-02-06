f3cbc92480c5f21221f338a2259f9c742454f158
sparrowsl <benthorpe.dev@gmail.com>
2024-02-06 08:15:03 +0000

    feat: display selected image on toggle thumbnails

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { For, createSignal } from "solid-js";
-import avatar from "/assets/images/image-avatar.png";
 import Header from "./components/Header.jsx";
+import { Thumbnail } from "./components/Thumbnail.jsx";
+
+export const [selectedImage, setSelectedImage] = createSignal(
+	"/assets/images/image-product-1.jpg"
+);
 
 export default function App() {
 	const [count, setCount] = createSignal(0);
@@ -10,14 +14,9 @@ export default function App() {
 			<Header />
 
 			<main class="container font-kumbhsans min-h-90vh flex flex-col justify-center">
-				{/* <Thumbnail /> */}
 				<article class="flex items-center px-20 gap-20">
 					<figure class="w-full max-w-25rem">
-						<img
-							src="/assets/images/image-product-1.jpg"
-							alt=""
-							class="max-w-25em rounded-xl"
-						/>
+						<img src={selectedImage()} alt="" class="max-w-25em rounded-xl" />
 						<figcaption class="grid grid-cols-4 gap-5 mt-7">
 							<For each={new Array(4)}>
 								{(_, i) => (
@@ -39,15 +38,6 @@ export default function App() {
 					</section>
 				</article>
 			</main>
-		</>
-	);
-}
-
-/** @param {Object{src: string}} props */
-export function Thumbnail(props) {
-	return (
-		<>
-			<img src={props.src} alt={props.alt || "Product"} class="rounded-lg" />
 		</>
 	);
 }
---
+++ src/components/Thumbnail.jsx
@@ -0,0 +1,21 @@
+import { selectedImage, setSelectedImage } from "../App.jsx";
+
+/** @param {Object{src: string}} props */
+export function Thumbnail(props) {
+	return (
+		<>
+			<button
+				type="button"
+				onClick={() => setSelectedImage(props.src)}
+				class="relative">
+				<img
+					src={props.src}
+					alt={props.alt || "Product"}
+					class={`${
+						selectedImage() === props.src && "border-orange border-3"
+					} rounded-lg`}
+				/>
+			</button>
+		</>
+	);
+}
