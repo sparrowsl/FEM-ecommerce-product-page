9aedaf6c66c8a597c0314aedc2bcf47f0fa99fd3
sparrowsl <benthorpe.dev@gmail.com>
2024-02-06 07:40:30 +0000

    display images and thumbnails

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createSignal } from "solid-js";
+import { For, createSignal } from "solid-js";
 import avatar from "/assets/images/image-avatar.png";
 import Header from "./components/Header.jsx";
 
@@ -9,22 +9,45 @@ export default function App() {
 		<>
 			<Header />
 
-			<main class="container font-kumbhsans min-h-90vh grid place-content-center">
-				<Thumbnail />
-				<h1 class="text-red">Vite + Solid</h1>
-				{/* biome-ignore lint/a11y/useButtonType: <explanation> */}
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count()}
-				</button>
+			<main class="container font-kumbhsans min-h-90vh flex flex-col justify-center">
+				{/* <Thumbnail /> */}
+				<article class="flex items-center px-20 gap-20">
+					<figure class="w-full max-w-25rem">
+						<img
+							src="/assets/images/image-product-1.jpg"
+							alt=""
+							class="max-w-25em rounded-xl"
+						/>
+						<figcaption class="grid grid-cols-4 gap-5 mt-7">
+							<For each={new Array(4)}>
+								{(_, i) => (
+									<Thumbnail
+										src={`/assets/images/image-product-${i() + 1}.jpg`}
+										alt={`Product ${i() + 1}`}
+									/>
+								)}
+							</For>
+						</figcaption>
+					</figure>
+
+					<section class="w-full">
+						<h1 class="text-red">Vite + Solid</h1>
+						{/* biome-ignore lint/a11y/useButtonType: <explanation> */}
+						<button onClick={() => setCount((count) => count + 1)}>
+							count is {count()}
+						</button>
+					</section>
+				</article>
 			</main>
 		</>
 	);
 }
 
-export function Thumbnail() {
+/** @param {Object{src: string}} props */
+export function Thumbnail(props) {
 	return (
 		<>
-			<img src={avatar} alt="" />
+			<img src={props.src} alt={props.alt || "Product"} class="rounded-lg" />
 		</>
 	);
 }
